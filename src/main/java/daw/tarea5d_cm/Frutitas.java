b1d2266124a9ffa5d39cfda8b4cdc1b28e80be4f
clara <calvara976@g.educaand.es>
2024-01-15 09:43:38 +0100

    Añade constructores en clase modelo

--- src/main/java/daw/tarea5d_cm/Frutitas.java
+++ src/main/java/daw/tarea5d_cm/Frutitas.java
@@ -13,4 +13,15 @@ public class Frutitas {
     private String codFrutita;
     private String nombre;
     private double peso;//kg
+    
+    //constructor por defecto
+    public Frutitas() {
+    }
+    //constructor parametizado
+    public Frutitas(String codFrutita, String nombre, double peso) {
+        this.codFrutita = codFrutita;
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+    
 }
