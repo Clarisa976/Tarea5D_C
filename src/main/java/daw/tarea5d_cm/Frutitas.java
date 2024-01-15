38438cbf0f09cdb3d0a1dedfb1b619ac5eb190ea
clara <calvara976@g.educaand.es>
2024-01-15 09:22:01 +0100

    Añade los atributos en clase frutita :D

---
+++ src/main/java/daw/tarea5d_cm/Frutitas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.tarea5d_cm;
+
+/**
+ *
+ * @author clara
+ */
+public class Frutitas {
+    
+    private String codFrutita;
+    private String nombre;
+    private double peso;//kg
+}
---
+++ src/main/java/daw/tarea5d_cm/Tarea5d_cm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw.tarea5d_cm;
+
+/**
+ *
+ * @author clara
+ */
+public class Tarea5d_cm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
