0263bc9ca0e5f53636d3dcc5209de30b37fb5c60
mavm0406 <mavm040601@gmail.com>
2024-01-15 10:12:20 +0100

    Hago main con 2 frutitas y las muestro

---
+++ src/main/java/daw/tarea5d_cm/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.tarea5d_cm;
+
+/**
+ *
+ * @author miguel
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Frutitas f1 = new Frutitas("1", "Fresa", 2.34);
+        Frutitas f2 = new Frutitas("2", "Manzana", 3.1);
+        
+        System.out.println(f1);
+        System.out.println(f2);
+    }
+}
